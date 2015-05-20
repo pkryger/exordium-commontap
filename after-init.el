5b37cdbd2f36f579862f81067359b69f362b295b
Przemek Kryger <pkryger@bloomberg.net>
2015-05-20 06:58:33 -0400

    Add revert all open buffers

--- after-init.el
+++ after-init.el
@@ -10,3 +10,16 @@
    (if (or force-reverting (not (buffer-modified-p)))
        (revert-buffer :ignore-auto :noconfirm)
      (error "The buffer has been modified"))))
+
+(defun revert-all-buffers ()
+  "Refreshes all open buffers from their respective files."
+  (interactive)
+  (dolist (buf (buffer-list))
+    (with-current-buffer buf
+      (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
+        (revert-buffer t t t) )))
+  (message "Refreshed open files.") )
+
+(global-set-key
+ (kbd "<C-f5>")
+ 'revert-all-buffers)
