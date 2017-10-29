fbe3eb75aca3e1cc33eee744a3222165524e3194
Przemyslaw Kryger <pkryger@gmail.com>
2017-10-29 11:24:55 +0000

    Revert "Add move region"
    This reverts commit 510edc30eb92e6f296b41dc0988ef95162aff218.

--- after-init.el
+++ after-init.el
@@ -49,26 +49,3 @@
       (rtags-start)
       (unless rtags-diagnostics-process
         (rtags-diagnostics)))))
-
-(defun move-region (start end n)
-  "Move the current region up or down by N lines."
-  (interactive "r\np")
-  (let ((line-text (delete-and-extract-region start end)))
-    (forward-line n)
-    (let ((start (point)))
-      (insert line-text)
-      (setq deactivate-mark nil)
-      (set-mark start))))
-
-(defun move-region-up (start end n)
-  "Move the current line up by N lines."
-  (interactive "r\np")
-  (move-region start end (if (null n) -1 (- n))))
-
-(defun move-region-down (start end n)
-  "Move the current line down by N lines."
-  (interactive "r\np")
-  (move-region start end (if (null n) 1 n)))
-
-(global-set-key (kbd "M-<up>") 'move-region-up)
-(global-set-key (kbd "M-<down>") 'move-region-down)
