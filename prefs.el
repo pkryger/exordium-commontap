c0df53572fa55cf0ece4105a9608278aeb0985a2
Przemek Kryger <pkryger@bloomberg.net>
2018-07-10 08:23:13 +0100

    Use spell check by default

--- prefs.el
+++ prefs.el
@@ -1,7 +1,7 @@
 (setq exordium-helm-everywhere t)
 (setq exordium-theme 'zenburn)
 (setq exordium-fci-mode :prog)
-(setq exordium-spell-check nil)
+;;(setq exordium-spell-check nil)
 (setq exordium-theme-use-big-org-fonts nil)
 ;;(setq exordium-smooth-scroll exordium-osx)
 (setq exordium-progress-bar t)
