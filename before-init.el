5e5009e13d12f18d6735d85a4ff5ebe6844b983a
Przemek Kryger <pkryger@bloomberg.net>
2018-07-19 09:03:11 +0100

    Add autopep8

--- before-init.el
+++ before-init.el
@@ -1,2 +1,3 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (add-to-list 'exordium-extra-pinned '(helpful . "melpa-pinned"))
+(add-to-list 'exordium-extra-pinned '(py-autopep8 . "melpa-pinned"))
