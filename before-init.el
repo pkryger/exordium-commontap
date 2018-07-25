579bceb05ce30627ded689b3aa176eccfd8db054
Przemek Kryger <pkryger@bloomberg.net>
2018-07-25 08:05:07 +0100

    Add deft

--- before-init.el
+++ before-init.el
@@ -1,3 +1,4 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (add-to-list 'exordium-extra-pinned '(helpful . "melpa-pinned"))
 (add-to-list 'exordium-extra-pinned '(py-autopep8 . "melpa-pinned"))
+(add-to-list 'exordium-extra-pinned '(deft . "melpa-pinned"))
