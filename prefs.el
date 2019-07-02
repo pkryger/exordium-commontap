c0cd0b13c3c4dd9b17047618fdea1c3b9d3a0443
Przemek Kryger <pkryger@bloomberg.net>
2019-07-02 08:59:42 +0100

    Switch to solarized

--- prefs.el
+++ prefs.el
@@ -1,5 +1,5 @@
 (setq exordium-helm-everywhere t)
-(setq exordium-theme 'zenburn)
+(setq exordium-theme 'solarized-light)
 (setq exordium-fci-mode :prog)
 ;;(setq exordium-spell-check nil)
 (setq exordium-theme-use-big-org-fonts nil)
