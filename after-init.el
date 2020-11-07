d0d13677824a106a07765dfe371e8a9d4ccab5d9
Przemyslaw Kryger <pkryger@gmail.com>
2020-11-07 13:10:39 +0000

    Remove buffer reverting - not using it anymore

--- after-init.el
+++ after-init.el
@@ -29,32 +29,6 @@
   (xterm-mouse-mode t)
   (defun track-mouse (e)))
 
-(global-set-key
- (kbd "<f5>")
- (lambda (&optional force-reverting)
-   "Interactive call to revert-buffer. Ignoring the auto-save
- file and not requesting for confirmation. When the current buffer
- is modified, the command refuses to revert it, unless you specify
- the optional argument: force-reverting to true."
-   (interactive "P")
-   ;;(message "force-reverting value is %s" force-reverting)
-   (if (or force-reverting (not (buffer-modified-p)))
-       (revert-buffer :ignore-auto :noconfirm)
-     (error "The buffer has been modified"))))
-
-(defun revert-all-buffers ()
-  "Refreshes all open buffers from their respective files."
-  (interactive)
-  (dolist (buf (buffer-list))
-    (with-current-buffer buf
-      (when (and (buffer-file-name) (file-exists-p (buffer-file-name)) (not (buffer-modified-p)))
-        (revert-buffer t t t) )))
-  (message "Refreshed open files.") )
-
-(global-set-key
- (kbd "<C-f5>")
- 'revert-all-buffers)
-
 (use-package use-package-ensure-system-package
   :ensure t)
 
