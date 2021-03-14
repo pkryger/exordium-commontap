9f7a0842ffec6b5405f0bd789c3253c3caba2cda
Przemyslaw Kryger <pkryger@gmail.com>
2021-03-14 15:53:39 +0000

    Delete superfluous before-init.el

--- before-init.el
+++
@@ -1,8 +0,0 @@
-;;
-
-(add-to-list 'exordium-extra-pinned '(deft . "melpa-pinned"))
-(add-to-list 'exordium-extra-pinned '(swiper . "melpa-pinned"))
-(add-to-list 'exordium-extra-pinned '(swiper-helm . "melpa-pinned"))
-(add-to-list 'exordium-extra-pinned '(helm-flyspell . "melpa-pinned"))
-
-;;
