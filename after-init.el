e087fc0bc6605b8c364b17277503b49aa503355d
Przemek Kryger <pkryger@gmail.com>
2023-05-18 14:56:07 +0100

    Tune apheleia to respect line length and ruff instead of isort

--- after-init.el
+++ after-init.el
@@ -1326,11 +1326,15 @@ Based on https://xenodium.com/emacs-dwim-do-what-i-mean/"
 (use-package protobuf-mode)
 
 (use-package apheleia
+  :custom
+  (apheleia-formatters-respect-fill-column t)
   :config
-  (setf (alist-get 'black apheleia-formatters)
-        '("black" "--line-length" (number-to-string fill-column) "-"))
+  (setf (alist-get 'isort apheleia-formatters)
+        '("isort" "--profile" "black" (apheleia-formatters-fill-column "--line-length") "-"))
+  (setf (alist-get 'ruff-isort apheleia-formatters)
+        '("ruff" "check" "--select" "I" "--exit-zero" "--fix" "--stdin-filename" filepath "-"))
   (setf (alist-get 'python-mode apheleia-mode-alist)
-        '(isort black)))
+        '(black ruff-isort)))
 
 ;; Cassandra - CQL support with sql-mode and ob-sql
 (require 'sql)
