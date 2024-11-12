1af86d76aa6cec9d1acb6e1eb921f3a8f1c3012e
Przemysław Kryger <pkryger@gmail.com>
2024-11-12 18:17:32 +0000

    Port settings from hometap

--- prefs.el
+++ prefs.el
@@ -1,3 +1,35 @@
+
+(setq exordium-preferred-frame-width  nil
+      exordium-preferred-frame-height nil)
+(setq exordium-preferred-fonts
+      '(("Fira Code" . 130)
+        ("Hack" . 130)
+        ("DejaVu Sans Mono for Powerline" . 130)
+        ("M+ 1mn" . 130)
+        ("M+ 1m" . 130)
+        ("Monaco" . 130)))
+
+(defcustom exordium-preferred-variable-fonts
+  '(("Fira Sans"  . 125))
+  ;; TODO: come up with a list of reasonable fonts
+  "TODO: List of preferred fonts/sizes to use, in decreasing order of
+preference. We will use the first one that is available on the
+local machine. It is a list of pairs (font-name . font-size). If
+nil, we don't set any font. Note that you can get the list of
+available font names by evaluating (font-family-list)."
+  ;; TODO::group 'exordium
+  :type  'sexp)
+
+(setq exordium-preferred-variable-fonts
+      '(("Fira Sans" . 125)))
+
+;; (custom-set-faces '(variable-pitch ((t (:family "Fira Sans" :height 135)))))
+
+;; (set-face-attribute 'default nil
+;;                     :family font
+;;                     :height size
+;;                     :weight 'normal)
+
 (setq exordium-desktop t)
 (setq exordium-helm-everywhere t)
 (setq exordium-theme nil)
