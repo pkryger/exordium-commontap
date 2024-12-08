c8d583bbb1d5a3041571ee8f4d2f9148263cbfe2
Przemysław Kryger <pkryger@gmail.com>
2024-12-08 15:26:24 +0000

    Rename init-vc-load-path to init-vc-checkout

--- init-vc-checkout.el
+++ init-vc-checkout.el
@@ -1,4 +1,4 @@
-;;; init-vc-load-path.el --- Install packages from VC -*- lexical-binding: t -*-
+;;; init-vc-checkout.el --- Install packages from VC -*- lexical-binding: t -*-
 
 ;;; Commentary:
 ;;
@@ -8,7 +8,7 @@
 (require 'use-package)
 (require 'vc)
 
-(defcustom exordium-use-package-vc-load-paths
+(defcustom exordium-vc-checkout-alist
   '((difftastic . "/Users/pkryger/gh/pkryger/difftastic.el"))
   "TODO:."
   :type '(alist :key-type (symbol :tag "Package")
@@ -24,21 +24,21 @@
              use-package-normalize-paths
              use-package-process-keywords))
 
-(defun use-package-normalize/:exordium-vc-load-path (_name keyword args)
+(defun use-package-normalize/:exordium-vc-checkout (_name keyword args)
                                         ; checkdoc-params: (keyword args)
   "Normalize possible arguments to the :exordium-vc."
   ;; TODO: handle empty string? (and in defaults)
   (use-package-only-one (symbol-name keyword) args
     #'use-package-normalize-paths))
 
-(defun exordium--vc-load-path-valid-p (dir)
+(defun exordium--vc-checkout-valid-p (dir)
   "Return non nil when DIR is good enough for checkout."
   (and (stringp dir)
        (not (equal "" dir))
        (file-directory-p dir)
        (vc-responsible-backend dir)))
 
-(defun exordium--vc-load-path-package-delete (name keyword dir)
+(defun exordium--vc-checkout-package-delete (name keyword dir)
                                         ; checkdoc-params: (keyword)
   "Delete package NAME if it has been previously installed from VC or ELPA.
 Avoid deletion when the package has been installed from VC into DIR."
@@ -64,7 +64,7 @@ Avoid deletion when the package has been installed from VC into DIR."
                      keyword (or kind "ELPA") name dir)
             (package-delete desc 'force)))))
 
-(defun use-package-handler/:exordium-vc-load-path (name keyword arg rest state)
+(defun use-package-handler/:exordium-vc-checkout (name keyword arg rest state)
                                         ; checkdoc-params: (keyword rest state)
   "VC Install package NAME from an existing checkout directory.
 Direcotory is (car ARG).  Package VC installation is done only if
@@ -75,15 +75,15 @@ that the directory can be prepended to :load-path's arguments to
 force :vc to eventually call `package-vc-install-from-checkout'
 with the directory."
   (if-let* ((dir (car arg))
-            ((exordium--vc-load-path-valid-p dir)))
+            ((exordium--vc-checkout-valid-p dir)))
       (use-package-concat
        ;; :load-path has been done, let's do as it does.
        `((eval-and-compile (add-to-list 'load-path ,dir)))
 
        ;;
        (if (bound-and-true-p byte-compile-current-file)
-           (funcall #'exordium--vc-load-path-package-delete name keyword dir)
-         `((exordium--vc-load-path-package-delete ',name ,keyword ,dir)))
+           (funcall #'exordium--vc-checkout-package-delete name keyword dir)
+         `((exordium--vc-checkout-package-delete ',name ,keyword ,dir)))
 
        ;; TODO: built-ins are `package-installed-p' too, so :vc won't install them
        (unless (plist-member rest :vc)
@@ -98,8 +98,8 @@ with the directory."
     (use-package-process-keywords name rest state)))
 
 (eval-after-load 'use-package-core
-  '(unless (memq :exordium-vc-load-path use-package-keywords)
-     (push :exordium-vc-load-path
+  '(unless (memq :exordium-vc-checkout use-package-keywords)
+     (push :exordium-vc-checkout
            (nthcdr
             (1+ (cl-position :load-path use-package-keywords))
             use-package-keywords))))
@@ -107,13 +107,13 @@ with the directory."
 
 ;; TODO: unlikely as we want to be affecting :load-path
 ;; (eval-after-load 'use-package-core
-;;   '(add-to-list 'use-package-keywords :exordium-vc-load-path))
+;;   '(add-to-list 'use-package-keywords :exordium-vc-checkout))
 
 (quote
  (macroexpand
   (quote
    (use-package difftastic
-     :exordium-vc-load-path "/Users/pkryger/gh/pkryger/difftastic.el"
+     :exordium-vc-checkout "/Users/pkryger/gh/pkryger/difftastic.el"
      :vc t
      :load-path "/foo/bar"))))
 
@@ -124,12 +124,12 @@ with the directory."
  (macroexpand
   (quote
    (use-package delight
-     :exordium-vc-load-path "/Users/pkryger/gh/savannah/delight"))))
+     :exordium-vc-checkout "/Users/pkryger/gh/savannah/delight"))))
 
-(defvar exordium--vc-load-path-orig-ensure-func
+(defvar exordium--vc-checkout-orig-ensure-func
   (nth 2 (assq :ensure use-package-defaults)))
 
-(defun exordium--vc-load-path-default-gate (func)
+(defun exordium--vc-checkout-default-gate (func)
   "Create a wrapper around FUNC to gate setting of default value.
 Wrapper is designed to be used around default gate function for
 keyword :ensure in `use-pakcage-defaults'.  It prevents setting
@@ -138,9 +138,9 @@ directory for a package NAME."
   (lambda (name args)
     (and (not (when-let* ((dir (or
                                 (alist-get name
-                                           exordium-use-package-vc-load-paths)
-                                (car (plist-get args :exordium-vc-load-path)))))
-                (exordium--vc-load-path-valid-p dir)))
+                                           exordium-vc-checkout-alist)
+                                (car (plist-get args :exordium-vc-checkout)))))
+                (exordium--vc-checkout-valid-p dir)))
          ;; Do as `use-package-process-keywords' does.
          (if (and func (functionp func))
              (funcall func name args)
@@ -148,67 +148,67 @@ directory for a package NAME."
 
 (eval-after-load 'use-package-core
   '(setf (nth 2 (assq :ensure use-package-defaults))
-         (exordium--vc-load-path-default-gate
-          exordium--vc-load-path-orig-ensure-func)))
+         (exordium--vc-checkout-default-gate
+          exordium--vc-checkout-orig-ensure-func)))
 
 (let ((use-package-always-ensure t)
-      (exordium-use-package-vc-load-paths '((difftastic . "/Users/pkryger/gh/pkryger/difftastic.el"))))
+      (exordium-vc-checkout-alist '((difftastic . "/Users/pkryger/gh/pkryger/difftastic.el"))))
   (not
-   (funcall (exordium--vc-load-path-default-gate
-             exordium--vc-load-path-orig-ensure-func)
+   (funcall (exordium--vc-checkout-default-gate
+             exordium--vc-checkout-orig-ensure-func)
             'difftastic nil)))
 
 (let ((use-package-always-ensure t)
-      (exordium-use-package-vc-load-paths '((difftastic . "/not-an-existing-path"))))
-  (funcall (exordium--vc-load-path-default-gate
-            exordium--vc-load-path-orig-ensure-func)
+      (exordium-vc-checkout-alist '((difftastic . "/not-an-existing-path"))))
+  (funcall (exordium--vc-checkout-default-gate
+            exordium--vc-checkout-orig-ensure-func)
            'difftastic nil))
 
 (let ((use-package-always-ensure t)
-      (exordium-use-package-vc-load-paths '((difftastic . t))))
-  (funcall (exordium--vc-load-path-default-gate
-            exordium--vc-load-path-orig-ensure-func)
+      (exordium-vc-checkout-alist '((difftastic . t))))
+  (funcall (exordium--vc-checkout-default-gate
+            exordium--vc-checkout-orig-ensure-func)
            'difftastic nil))
 
 (let ((use-package-always-ensure t)
-      (exordium-use-package-vc-load-paths nil))
+      (exordium-vc-checkout-alist nil))
   (not
-   (funcall (exordium--vc-load-path-default-gate
-             exordium--vc-load-path-orig-ensure-func)
-            'difftastic '(:exordium-vc-load-path
+   (funcall (exordium--vc-checkout-default-gate
+             exordium--vc-checkout-orig-ensure-func)
+            'difftastic '(:exordium-vc-checkout
                           "/Users/pkryger/gh/pkryger/difftastic.el"))))
 
 (let ((use-package-always-ensure t)
-      (exordium-use-package-vc-load-paths nil))
-   (funcall (exordium--vc-load-path-default-gate
-                       exordium--vc-load-path-orig-ensure-func)
-                      'difftastic '(:exordium-vc-load-path
+      (exordium-vc-checkout-alist nil))
+   (funcall (exordium--vc-checkout-default-gate
+                       exordium--vc-checkout-orig-ensure-func)
+                      'difftastic '(:exordium-vc-checkout
                                     "/not-an-existing-path")))
 
 (let ((use-package-always-ensure t)
-      (exordium-use-package-vc-load-paths nil))
-  (funcall (exordium--vc-load-path-default-gate
-            exordium--vc-load-path-orig-ensure-func)
-           'difftastic '(:exordium-vc-load-path
+      (exordium-vc-checkout-alist nil))
+  (funcall (exordium--vc-checkout-default-gate
+            exordium--vc-checkout-orig-ensure-func)
+           'difftastic '(:exordium-vc-checkout
                          t)))
 
 (defvar pk/orig-use-package-defaults use-package-defaults)
 
 (eval-after-load 'use-package-core
-  '(setf (alist-get :exordium-vc-load-path use-package-defaults)
+  '(setf (alist-get :exordium-vc-checkout use-package-defaults)
     '((lambda (name _args)
-        (use-package-normalize/:exordium-vc-load-path
+        (use-package-normalize/:exordium-vc-checkout
          name
-         :exordium-vc-load-path
-         (list (alist-get name exordium-use-package-vc-load-paths))))
+         :exordium-vc-checkout
+         (list (alist-get name exordium-vc-checkout-alist))))
       (lambda (name args)
         (and (when-let* ((dir (alist-get name
-                                         exordium-use-package-vc-load-paths))
-                         ((exordium--vc-load-path-valid-p dir)))
-             (not (plist-member args :exordium-vc-load-path)))))))
+                                         exordium-vc-checkout-alist))
+                         ((exordium--vc-checkout-valid-p dir)))
+             (not (plist-member args :exordium-vc-checkout)))))))
 
 
 
-(provide 'init-vc-load-path)
+(provide 'init-vc-checkout)
 
-;;; init-vc-load-path.el ends here
+;;; init-vc-checkout.el ends here
