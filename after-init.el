cbed524bcde5761828a8784f91ee717d8ea648d8
Przemysław Kryger <pkryger@gmail.com>
2025-01-23 12:49:22 +0000

    Evil: remove obsolete suppresion code

--- after-init.el
+++ after-init.el
@@ -1477,21 +1477,6 @@ language."
   (tab-bar-mode))
 
 
-
-;; evil likes to turn itself on, let's disable it so no surprises
-;; (use-package evil
-;;   :init
-;;   (defun pk/disable-evil-mode ()
-;;     (unless exordium-enable-evil-mode
-;;       (when (or evil-mode evil-local-mode)
-;;         (turn-off-evil-mode))))
-;;   :hook
-;;   ((evil-mode . pk/disable-evil-mode)
-;;    (evil-local-mode . pk/disable-evil-mode))
-;;   :config
-;;   (advice-remove #'select-window #'ad-Advice-select-window))
-
-
 (use-package so-long
   :init
   (defun pk/so-long-skip-bidi-inhibit-bpa-override ()
