f9ce40d47f813ba928671980277e1e992c582018
Przemysław Kryger <pkryger@gmail.com>
2025-02-19 18:07:37 +0000

    feat: Disable which-key-mode

--- prefs.el
+++ prefs.el
@@ -72,7 +72,7 @@ available font names by evaluating (font-family-list)."
 ;; orderless prevents using fast `helm-source-in-buffer' and the
 ;; annotation/affixation search (&) doesn't work anyway
 (setq exordium-helm-completion-style 'helm)
-
+(setq exordium-enable-which-key nil)
 (setq exordium-split-window-preffered-direction 'longest)
 (setq exordium-desktop t)
 (setq exordium-helm-everywhere t)
