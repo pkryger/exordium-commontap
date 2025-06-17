bee4e8a46d7137cdc311b1c7183192e46783f741
Przemysław Kryger <pkryger@gmail.com>
2025-06-17 21:33:26 +0100

    feat: add #bug67604 to debbugs

--- after-init.el
+++ after-init.el
@@ -2170,6 +2170,7 @@ would move point to an (partially) invisible line."
   (debbugs-gnu-default-bug-number-list
    (string-join
     (list
+     (propertize "67604" 'help-echo "Motion problems with inline images")
      (propertize "77944" 'help-echo "31.0.50; M-x man on macOS fails with error")
      (propertize "78766" 'help-echo "100-4000x redisplay slowdown with vscroll>0 and make-cursor-line-fully-visible=t")
      )
