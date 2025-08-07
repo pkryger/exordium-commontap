4bf66f19927cdd97ba7558baae2267bc24519f36
Przemysław Kryger <pkryger@gmail.com>
2025-08-07 12:41:01 +0100

    feat: Add bug#79188 to debbugs

--- after-init.el
+++ after-init.el
@@ -2240,6 +2240,7 @@ would move point to an (partially) invisible line."
      (propertize "77944" 'help-echo "31.0.50; M-x man on macOS fails with error")
      (propertize "78712" 'help-echo "31.0.50; definition is void in file-notify")
      (propertize "78766" 'help-echo "100-4000x redisplay slowdown with vscroll>0 and make-cursor-line-fully-visible=t")
+     (propertize "79188" 'help-echo "Cannot build packages installed from VC")
      )
     ",")))
 
